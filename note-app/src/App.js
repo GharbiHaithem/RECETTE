ff17dd1e05bf836ed84833401ba2278866b73eca
GharbiHaithem <gharbi.haythem1988@gmail.com>
2023-06-14 16:59:05 +0200

    first commit

--- note-app/src/App.js
+++ note-app/src/App.js
@@ -31,7 +31,7 @@ if(userstate)
 useEffect(()=>{
   const getUser = async()=>{
     try{
-  const url = `${base_url}/auth/login/success`;
+  const url = `http://localhost:5000/api/auth/login/success`;
   
   const response = await axios.get(url,{withCredentials:true})
   console.log(response)
