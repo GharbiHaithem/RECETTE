b633a3285dd233ab2045f1b3445c32e62dcaa773
GharbiHaithem <gharbi.haythem1988@gmail.com>
2023-06-14 07:54:33 +0200

    commit to deploy2

--- note-app/src/App.js
+++ note-app/src/App.js
@@ -30,7 +30,7 @@ if(userstate)
 useEffect(()=>{
   const getUser = async()=>{
     try{
-  const url = `https://recette-crud.onrender.com/api/auth/login/success`;
+  const url = `http://localhost:5000/api/auth/login/success`;
   const response = await axios.get(url,{withCredentials:true})
   console.log(response)
    setUser(response.data.user)
