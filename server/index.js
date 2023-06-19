05944d22e15997b78add835d079dfca5bcf61d22
GharbiHaithem <gharbi.haythem1988@gmail.com>
2023-06-19 19:11:09 +0200

    SIGN UP GOOGLE PASSPORT

--- server/index.js
+++ server/index.js
@@ -54,7 +54,7 @@ app.use(passport.initialize())
 
 app.use(passport.session())
 app.use(cors({
-    origin:"http://localhost:3000",
+    origin:"https://recette-beta.vercel.app",
     methods:"GET,POST,PUT,DELETE",
     credentials:true
 }))
